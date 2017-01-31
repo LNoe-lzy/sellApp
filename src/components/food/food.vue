1d970dad753dab28b0337defccbf0abb38240073
LNoe-lzy <m15222505516@163.com>
2017-01-31 16:11:09 +0800

    完善商品详情页

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -18,11 +18,22 @@
             <span class="now">¥ {{food.price}}</span>
             <span class="old" v-show="food.oldPrice">¥ {{food.oldPrice}}</span>
           </div>
+          <div class="cartcontrol-wrapper">
+            <Cartcontrol :food="food"></Cartcontrol>
+          </div>
+          <div @click="addFirst" class="buy" v-show="!food.count || food.count === 0">加入购物车</div>
         </div>
-        <div class="cartcontrol-wrapper">
-          <Cartcontrol :food="food"></Cartcontrol>
+        <Split v-show="food.info"></Split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
         </div>
-        <div @click="addFirst" class="buy" v-show="!food.count || food.count === 0">加入购物车</div>
+        <Split></Split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <Ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc"
+                        :ratings="food.ratings"></Ratingselect>
+        </div>
       </div>
     </div>
   </transition>
@@ -32,8 +43,13 @@
   import Vue from 'vue';
   import BScroll from 'better-scroll';
   import Cartcontrol from '../cartcontrol/cartcontrol.vue';
+  import Split from '../split/split.vue';
+  import Ratingselect from '../ratingselect/ratingselect.vue';
   import eventHub from '../../eventhub';
-  
+  //  const POSITIVE = 0;
+  //  const NEGATIVE = 1;
+  const ALL = 2;
+
   export default {
     props: {
       food: {
@@ -42,12 +58,21 @@
     },
     data () {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       };
     },
     methods: {
       show () {
         this.showFlag = true;
+        this.selectType = ALL;
+        this.onlyContent = false;
         this.$nextTick(() => {
           if (!this.scroll) {
             this.scroll = new BScroll(this.$refs.food, {
@@ -70,7 +95,9 @@
       }
     },
     components: {
-      Cartcontrol
+      Cartcontrol,
+      Split,
+      Ratingselect
     }
   };
 </script>
@@ -113,6 +140,7 @@
 
     .content
       padding: 18px
+      position: relative
       .title
         line-height: 14px
         margin-bottom: 8px
@@ -140,21 +168,41 @@
           text-decoration: line-through
           font-size: 10px
           color: rgb(147, 153, 159)
-    .cartcontrol-wrapper
-      position: absolute
-      right: 12px
-      bottom: 12px
-    .buy
-      position: absolute
-      right: 18px
-      bottom: 18px
-      z-index: 10
-      height: 24px
-      line-height: 24px
-      padding: 0 12px
-      box-sizing: border-box
-      font-size: 10px
-      border-radius: 12px
-      color: #fff
-      background: rgb(0, 160, 220)
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        font-size: 10px
+        border-radius: 12px
+        color: #fff
+        background: rgb(0, 160, 220)
+
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rgb(7, 17, 27)
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: 12px
+        color: rgb(77, 85, 93)
+    .rating
+      padding-top: 18px
+      .title
+        line-height: 14px
+        margin-left: 18px
+        font-size: 14px
+        color: rgb(7, 17, 27)
 </style>
